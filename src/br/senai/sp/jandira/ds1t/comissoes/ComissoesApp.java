c5388c3e8f4dcae87b619cb1d2fd37f1fa2c0a66
EVELLYN SANTIAGO DE SOUZA <25203674@CFP127ALUNO.LOCAL>
2025-09-22 13:56:12 -0300

    Finalizado

--- src/br/senai/sp/jandira/ds1t/comissoes/ComissoesApp.java
+++ src/br/senai/sp/jandira/ds1t/comissoes/ComissoesApp.java
@@ -1,4 +1,23 @@
 package br.senai.sp.jandira.ds1t.comissoes;
 
+import br.senai.sp.jandira.ds1t.comissoes.model.Vendedor;
+
 public class ComissoesApp {
+    public static void main(String[] args){
+            System.out.println("Iniando programa...");
+
+            Vendedor vendedor = new Vendedor();
+            vendedor.ColetarDados();
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/br/senai/sp/jandira/ds1t/comissoes/model/Vendedor.java
+++ src/br/senai/sp/jandira/ds1t/comissoes/model/Vendedor.java
@@ -1,4 +1,48 @@
 package br.senai.sp.jandira.ds1t.comissoes.model;
 
+import java.util.Scanner;
+
 public class Vendedor {
+
+    String nomeVendedor;
+    double totalVedas;
+    double percentualComissao;
+    double valorComissao;
+
+    public void ColetarDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        // obtendo os dados dos vendedores
+        System.out.println("** CALCULADORA DE COMISSOES**");
+        System.out.println("--------------------------------");
+
+        // Obtendo o nome do vendedor
+        System.out.println("Nome do vendedor");
+        nomeVendedor = leitor.nextLine();
+
+        //Obtendo o total de vendas do vendedor
+        System.out.println("Total de vendas do(a) " + nomeVendedor + ":");
+        totalVedas = leitor.nextDouble();
+        calcularComissao();
+    }
+
+    public void calcularComissao(){
+        if(totalVedas < 20000){
+           percentualComissao = 5;
+        } else {
+            percentualComissao = 10;
+        }
+
+        valorComissao = totalVedas * percentualComissao / 100;
+        exibirDados();
+    }
+
+        public void exibirDados(){
+        System.out.println("------------------");
+        System.out.println("NOME DO VENDEDOR: " + nomeVendedor);
+        System.out.println("TOTAL DE VENDAS: R$" + totalVedas );
+        System.out.println("PERCENTUAL DE COMISSAO: " + percentualComissao);
+        System.out.println("VALOR DA COMISSÃO: R$ " + valorComissao);
+        }
+
 }
